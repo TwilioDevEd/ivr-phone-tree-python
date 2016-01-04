2952c32af21c8b972f4e2aa81e0b3629c1851ae0
nebtrx <neb.trx@gmail.com>
2016-01-04 15:34:13 -0500

    Remove unnecessary comment

--- ivr_phone_tree_python/__init__.py
+++ ivr_phone_tree_python/__init__.py
@@ -3,7 +3,6 @@ from ivr_phone_tree_python.config import config_env_files
 
 
 def configure_app(new_app, config_name='development'):
-    # new_app = Flask(__name__)
     new_app.config.from_object(config_env_files[config_name])
 
 

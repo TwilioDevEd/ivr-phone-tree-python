7487708f50241b405b858d5cd7a3363aaa4a2f0c
nebtrx <neb.trx@gmail.com>
2016-01-04 15:41:55 -0500

    Fix code format

--- ivr_phone_tree_python/__init__.py
+++ ivr_phone_tree_python/__init__.py
@@ -5,7 +5,6 @@ from ivr_phone_tree_python.config import config_env_files
 def configure_app(new_app, config_name='development'):
     new_app.config.from_object(config_env_files[config_name])
 
-
 app = Flask(__name__)
 import ivr_phone_tree_python.views
 
